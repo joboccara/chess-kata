de3a492456695a15329c1ab9422476e7afc123d3
joboccara <boccaraj@gmail.com>
2022-06-13 15:54:32 +0200

    extract move in its file

---
+++ lib/move.rb
@@ -0,0 +1,34 @@
+class Move
+  def initialize(from, to)
+    @from = from
+    @to = to
+  end
+
+  def horizontal?
+    (@to.y - @from.y).abs == 0
+  end
+
+  def vertical?
+    (@to.x - @from.x).abs == 0
+  end
+
+  def diagonal?
+    (@to.y - @from.y).abs == (@to.x - @from.x).abs
+  end
+
+  def hovered_positions
+    x_direction = @to.x <=> @from.x
+    y_direction = @to.y <=> @from.y
+    (1...distance).map{ |i| Position.new(@from.x + i * x_direction, @from.y + i * y_direction) }
+  end
+
+  def distance
+    if horizontal?
+      (@to.x - @from.x).abs
+    elsif vertical?
+      (@to.y - @from.y).abs
+    elsif diagonal?
+      (@to.y - @from.y).abs
+    end
+  end
+end
