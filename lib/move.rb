173b08844c385dc88045a1e0351de5e0a0f1836a
joboccara <boccaraj@gmail.com>
2022-06-16 11:15:10 +0200

    refactoring

--- lib/move.rb
+++ lib/move.rb
@@ -7,42 +7,42 @@ class Move
   end
 
   def horizontal?
-    (@to.y - @from.y).abs == 0
+    (to.y - from.y).abs == 0
   end
 
   def vertical?
-    (@to.x - @from.x).abs == 0
+    (to.x - from.x).abs == 0
   end
 
   def diagonal?
-    (@to.y - @from.y).abs == (@to.x - @from.x).abs
+    (to.y - from.y).abs == (to.x - from.x).abs
   end
 
   def l?
-    x_distance = (@to.x - @from.x).abs
-    y_distance = (@to.y - @from.y).abs
+    x_distance = (to.x - from.x).abs
+    y_distance = (to.y - from.y).abs
     [1, 2].include?(x_distance) && x_distance + y_distance == 3
   end
 
   def forward?
-    @to.y > @from.y
+    to.y > from.y
   end
 
   def distance
     if horizontal?
-      (@to.x - @from.x).abs
+      (to.x - from.x).abs
     elsif vertical?
-      (@to.y - @from.y).abs
+      (to.y - from.y).abs
     elsif diagonal?
-      (@to.y - @from.y).abs
+      (to.y - from.y).abs
     elsif l?
       1
     end
   end
 
   def hovered_positions
-    x_direction = @to.x <=> @from.x
-    y_direction = @to.y <=> @from.y
-    (1...distance).map{ |i| Position.new(@from.x + i * x_direction, @from.y + i * y_direction) }
+    x_direction = to.x <=> from.x
+    y_direction = to.y <=> from.y
+    (1...distance).map{ |i| Position.new(from.x + i * x_direction, from.y + i * y_direction) }
   end
 end
